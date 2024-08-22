f16d54109737ee98ec317295973bbde8ab034c16
Keshav Sharma <keshavvsharma.09@gmail.com>
2024-08-22 06:25:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"Use Strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs ,not browser
+
+console.log(3
+    + 
+    3) //thats not a good way, code readability should be high 
+
+console.log("Sharma")
+
+let name = "Keshav" //String data type
+let age = 21 //number without double quote
+let isLoggedIn = false //boolean type
+let state = null ;
+
+//Types of premitive data types
+// number =>  2 to power 53
+// bigint => for large number values
+// string => ""
+// boolean => true/false
+// null => standalone value /means not assigned, empty value
+// undefined =>
+// symbol => for uniqueness
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
