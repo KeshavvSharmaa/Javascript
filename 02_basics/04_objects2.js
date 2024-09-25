bd4bd0e2faeb5770e6fb87a5f0344d58796c9248
Keshav Sharma <keshavvsharma.09@gmail.com>
2024-09-25 13:27:22 +0000

    objects two

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,40 @@
+// nconst tinderUser = new Object() // singleton object
+const tinderUser = {} // non-singleton object, otherwise both are same as there is no internal difference
+tinderUser.id = "123abc"
+tinderUser.name ="Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {             // can do nesting
+            firstname: "Keshav",
+            lastname: "Sharma"
+        }
+    }
+
+}
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"e", 6:"f"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // {} is optional parameter which copies the data into the target source
+const obj3 = {...obj1, ...obj2} //spread operator ...
+// console.log(obj3);
+
+console.log(Objects.keys(tinderUser)); // returns all the keys and the output will be in array
+console.log(Objects.values(tinderUser)); //returns only all the values without the keys
+console.log(Objects.entries(tinderUser)); //returns keys and their values
+
+console.log(tinderUser.hasownproperty('isLoggedIn')); // to check whether it is available or not and tells in boolean
+
+
+
