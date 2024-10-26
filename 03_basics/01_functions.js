a7bef329de0b75f89b5dfb395d9e0c4c4ab286f6
Keshav Sharma <keshavvsharma.09@gmail.com>
2024-10-26 04:44:12 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,34 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("Keshav"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){ // rest and spread operator
+       return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 600, 800)) #val1 = 200, val2 = 400 and the rest will go to num1 i.e 600 , 800
+
+const user = {
+  username: "Keshav",
+  price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 700]
+
+function returnSecondValue(getArray){
+      return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 900]));
